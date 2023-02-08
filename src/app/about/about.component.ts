104e66ba72d2048f6a097f39efb826145aa45a59
shereink <shereinkorim@gmail.com>
2023-02-08 18:42:50 +0200

    lab1

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  img_dowenload:string = "./assets/imgs/WhatsApp Image 2023-02-06 at 13.55.32.jpeg"
+
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  img_header : string = "./assets/imgs/WhatsApp Image 2023-02-06 at 13.55.32.jpeg"
+
+}
